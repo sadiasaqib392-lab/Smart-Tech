a806071178b4e85203ff05cf4de02f26d8bef92e
sadiasaqib392-lab <sadiasaqib392@gmail.com>
2026-09-23 15:38:47 +0500

    smarttech

--- src/data/companyData.ts
+++ src/data/companyData.ts
@@ -34,7 +34,7 @@ export const COMPANY_INFO = {
   phoneRaw: '+923344205974',
   directorPhone: '+923130477583',
   directorPhoneRaw: '+923130477583',
-  email: 'smartech6@gmail.com',
+  email: 'smartechelectrical6@gmail.com',
   whatsappNumber: '923344205974',
   defaultWhatsAppMessage: 'Hello Smartech, I would like to discuss a technical project/requirement. Please provide more information.',
 };
